06f83c282ab7c037912c83752e00b2ddca1983af
Alekhya Nallabati <alekhya.nallabati@gmail.com>
2024-05-14 16:35:09 +0530

    miscellineous

---
+++ xxx.py
@@ -0,0 +1,42 @@
+# baseline = {'music': 'bach', 'art': 'rembrandt'}
+# adjustments = {'art': 'van gogh', 'opera': 'carmen'}
+#
+# combined = baseline.copy()
+# combined.update(adjustments)
+# print(list(combined))
+
+# i = 0
+# while i < 3:
+#        print(i)
+#        i += 1
+# else:
+#       print(0)
+
+# print([[i+j for i in "abc"] for j in "def"])
+
+# i = 0
+# while i < 5:
+#     print(i)
+#     i += 1
+#     if i == 3:
+#         break
+# else:
+#     print(0)
+
+# i = 1
+# while True:
+#     if i % 3 == 0:
+#         break
+#     print(i)
+#
+#     i += 1
+
+# fruits = ["Apple", "Banana", "Cherry"]
+# for i, fruit in enumerate(fruits):
+#  if i == 1:
+#   print(fruit)
+
+a = 2
+b = 4
+c = a ^ b
+print(c)
